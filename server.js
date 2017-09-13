bf0294558a994977889a8c17cc14118c464f6a54
Xiong Vang <vang0641@gmail.com>
2017-09-13 15:03:14 -0500

    restructing the tree and connect server with client

--- server.js
+++ server.js
@@ -11,3 +11,7 @@ app.use(express.static('public'));
 app.listen(port, function(){
   console.log('listening on port', port);
 });
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/views/index.html'));
+});
