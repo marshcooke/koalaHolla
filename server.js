55d41ecedcdbed4a5c7575ffd4d9a4dff02242f1
Xiong Vang <vang0641@gmail.com>
2017-09-13 14:48:37 -0500

    server spun

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var path = require( 'path' );
-var port = process.env.PORT || 5000;
+var port = 5000;
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('server/public'));
+app.use(express.static('public'));
 
 // Start listening for requests on a specific port
 app.listen(port, function(){
